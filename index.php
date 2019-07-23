eb2b269f8e8433ed44b484de3256151f464ca092
Code9 <Code9@delibrary.priv>
2019-07-23 16:06:58 -0400

    update

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+get_header();
+?>
+
+<h1> this is the top of my page</h1>
+
+<div class="mejs-inner">
+    <div class="mejs-mediaelement">
+        <mediaelementwrapper id="mejs_41507690104097916">
+            <video
+                loop="loop"
+                autoplay=""
+                playsinline=""
+                muted=""
+                id="mejs_41507690104097916_html5"
+                preload="none"
+                src="http://teenwarehouse.org/wp-content/uploads/Final-Teen-Warehouse-3-22-2019...-1.mp4"
+                style="width: 983.111px; height: 553px; margin: 0px;">
+				<source type="video/mp4" src="http://teenwarehouse.org/wp-content/uploads/Final-Teen-Warehouse-3-22-2019...-1.mp4">
+				<source type="video/webm" src="http://teenwarehouse.org/wp-content/uploads/Final-Teen-Warehouse-3-22-2019...-1....webm">
+            </video>
+        </mediaelementwrapper>
+    </div>
+    <div class="mejs-layers">
+        <div class="mejs-poster mejs-layer" style="display: none; width: 1280px; height: 720px;">
+
+        </div>
+        <div class="mejs-overlay mejs-layer" style="width: 1280px; height: 720px; display: none;">
+            <div class="mejs-overlay-loading"><span class="mejs-overlay-loading-bg-img">
+
+            </span>
+        </div>
+    </div>
+    <div class="mejs-overlay mejs-layer" style="display: none; width: 1280px; height: 720px;">
+        <div class="mejs-overlay-error">
+
+        </div>
+    </div>
+        <div class="mejs-overlay mejs-layer mejs-overlay-play" style="width: 1280px; height: 720px; display: none;">
+            <div class="mejs-overlay-button" role="button" tabindex="0" aria-label="Play" aria-pressed="false">
+
+            </div>
+        </div>
+    </div>
+    <div class="mejs-controls mejs-offscreen" style="opacity: 0;">
+        <div class="mejs-button mejs-playpause-button mejs-pause">
+            <button type="button" aria-controls="mep_0" title="Pause" aria-label="Pause" tabindex="0">
+
+            </button>
+        </div>
+        <div class="mejs-time mejs-currenttime-container" role="timer" aria-live="off">
+            <span class="mejs-currenttime">00:00</span></div><div class="mejs-time-rail">
+                <span class="mejs-time-total mejs-time-slider" role="slider" tabindex="0">
+                    <span class="mejs-time-buffering" style="display: none;">
+
+                    </span><span class="mejs-time-loaded" style="transform: scaleX(1);">
+
+                    </span><span class="mejs-time-current" style="transform: scaleX(0.31);">
+
+                    </span><span class="mejs-time-hovered no-hover">
+
+                    </span><span class="mejs-time-handle" style="transform: translateX(31px);"><span class="mejs-time-handle-content">
+
+                    </span></span><span class="mejs-time-float">
+                        <span class="mejs-time-float-current">00:00</span>
+                        <span class="mejs-time-float-corner"></span>
+                    </span>
+                </span>
+            </div>
+                <div class="mejs-time mejs-duration-container">
+                    <span class="mejs-duration">00:16</span></div
+                    ><div class="mejs-button mejs-volume-button mejs-unmute">
+                        <button type="button" aria-controls="mep_0" title="Unmute" aria-label="Unmute" tabindex="0">
+
+                        </button>
+                        <a href="javascript:void(0);" class="mejs-volume-slider" aria-label="Volume Slider" aria-valuemin="0" aria-valuemax="100" role="slider" aria-orientation="vertical" aria-valuenow="0" aria-valuetext="0%">
+                            <span class="mejs-offscreen">Use Up/Down Arrow keys to increase or decrease volume.</span>
+                            <div class="mejs-volume-total"><div class="mejs-volume-current" style="bottom: 0px; height: 0%;"></div><div class="mejs-volume-handle" style="bottom: 0%; margin-bottom: -3px;">
+
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="mejs-button mejs-fullscreen-button">
+                    <button type="button" aria-controls="mep_0" title="Fullscreen" aria-label="Fullscreen" tabindex="0">
+
+                    </button>
+                </div>
+            </div>
+        </div>
